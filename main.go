8dcd0572ac35d88a0d7e13315d83c05e0a5d7aa8
pixelice-dev <pxlc@pm.me>
2022-10-13 13:05:17 +0800

    Format code using go fmt extension in vscode

--- main.go
+++ main.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-    "log"
+	"log"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    // connect to DB first
-    env := new(Env)
-    var err error
-    env.DB, err = ConnectDB()
-    if err != nil {
-        log.Fatalf("failed to start the server: %v", err)
-    }
+	// connect to DB first
+	env := new(Env)
+	var err error
+	env.DB, err = ConnectDB()
+	if err != nil {
+		log.Fatalf("failed to start the server: %v", err)
+	}
 
-    router := gin.Default()
-    router.GET("/albums/:id", env.GetAlbumByID)
-    router.GET("/albums", env.GetAlbums)
-    router.POST("/albums", env.PostAlbum)
-    router.PUT("/albums", env.UpdateAlbum)
-    router.DELETE("/albums/:id", env.DeleteAlbumByID)
+	router := gin.Default()
+	router.GET("/albums/:id", env.GetAlbumByID)
+	router.GET("/albums", env.GetAlbums)
+	router.POST("/albums", env.PostAlbum)
+	router.PUT("/albums", env.UpdateAlbum)
+	router.DELETE("/albums/:id", env.DeleteAlbumByID)
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -28,5 +28,5 @@ func main() {
 		})
 	})
 
-    router.Run()
+	router.Run()
 }
--- utils.go
+++ utils.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func makeGinResponse(c *gin.Context, statusCode int, value string) {
-    c.JSON(statusCode, gin.H{
-        "message":    value,
-        "statusCode": statusCode,
-    })
+	c.JSON(statusCode, gin.H{
+		"message":    value,
+		"statusCode": statusCode,
+	})
 }
-
